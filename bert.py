e08cb3defd273b6b0a64065036cac2b3d09f716c
Tanio <phuongthanh02052003@gmail.com>
2023-09-19 11:29:40 +0700

    fix bertlayer

--- bert.py
+++ bert.py
@@ -2,24 +2,24 @@ import torch
 import torch.nn as nn
 import math
 class BertAttention(nn.Module):
-    def __init__(self, config):
+    def __init__(self, config, masked_attention = None):
         super(BertAttention,self).__init__(config)
+        assert config.hidden_size%config.num_heads == 0
         self.num_heads = config.num_heads
         self.head_size = self.hidden_size//self.num_heads
-        self.total_head_size = self.num_heads*self.head_size
-        self.query = nn.Linear(config.hidden_size, config.total_head_size)
-        self.key = nn.Linear(config.hidden_size, config.total_head_size)
-        self.value = nn.Linear(config.hidden_size, config.total_head_size)
-        self.proj = nn.Linear(config.total_head_size, config.fhsize)
+        self.query = nn.Linear(config.hidden_size, config.hidden_size)
+        self.key = nn.Linear(config.hidden_size, config.hidden_size)
+        self.value = nn.Linear(config.hidden_size, config.hidden_size)
         self.softmax = nn.Softmax(dim = -1)
         self.do = nn.Dropout( p = config.dropout_rate)
+        self.masked_attention = masked_attention
     def transform(self, x, f):
         bs, sen_len = x[:2]
         proj = f(x)
         proj = proj.view(bs,sen_len,self.num_heads,self.head_size)
         proj = proj.permute(0,2,1,3)
         return proj
-    def attention(self, xq, xk, xv, masked_attention):
+    def attention(self, xq, xk, xv):
         # B: batch size
         # L: source sequence length
         # T: target sequence length
@@ -32,7 +32,7 @@ class BertAttention(nn.Module):
         assert xv.shape = _, self.num_heads, _, self.head_size
         B, _, L, _ = xq.shape
         attention_score = xq.bmm(xk)/math.sqrt(self.head_size) # (B, H, L, T)
-        if masked_attention is not None:
+        if self.masked_attention is not None:
             #masked_attention: (B,1,L,T)
             masked_attention = 1.0 - masked_attention
             masked_attention *= -1e5
@@ -40,7 +40,7 @@ class BertAttention(nn.Module):
         attention_score = self.softmax(attention_score)
         attention_score = self.dropout(attention_score) # B, H, L, D
         attention_score = attention_score.permute(0,2,1,3).view(B,L,-1) #B, L, E
-        attention_score = self.proj(attention_score) #b, l, fhsize
+        return attention
     def forward(self, x):
         xq = transform(x, self.query)
         xk = transform(x, self.key)
@@ -51,17 +51,19 @@ class BertLayer(nn.Module):
     def __init__(self, config):
         super.__init__(config)
         self.MultiheadAttention = BertAttention(config)
-        self.norm = nn.LayerNorm(config.fhsize)
-        self.ff = nn.Sequential(nn.Linear(config.fhsize, config.ffpro1, bias = True),
-                                nn.ReLU(),
-                                nn.Linear(config.ffpro1, config.ffpro2, bias = True),
+        self.attention_dense_layer = nn.Linear(config.hidden_size, config.hidden_size)
+        self.norm = nn.LayerNorm(config.hidden_size)
+        self.ff = nn.Sequential(nn.Linear(config.hidden_size, config.intermediate_size, bias = True),
+                                nn.GELU(),
+                                nn.Linear(config.intermediate_size, config.hidden_size, bias = True),
                                )
     def forward(self, x):
-        x = x + self.MultiheadAttention(x)
-        x = self.norm(x)
-        x = x+ self.ff(x)
-        x = self.norm(x)
-        return x
+        f = self.MultiheadAttention(x)
+        f = self.dropout(self.attention_dense_layer(f))
+        out = self.norm(x+f)
+        f =self.dropout(self.ff(out))
+        out = self.norm(out+f)
+        return out
 class BertModel(nn.Module):
     def __init__(config):
         super.__init__(config)
